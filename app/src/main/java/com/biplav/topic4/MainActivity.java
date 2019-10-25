758201d128784ba3e65b1b7d69f22f500917cdfb
BaxanAcharya <baxanacharya@gmail.com>
2019-10-25 10:20:52 +0545

    List  view country addede

--- app/src/main/java/com/biplav/topic4/MainActivity.java
+++ app/src/main/java/com/biplav/topic4/MainActivity.java
@@ -3,12 +3,24 @@ package com.biplav.topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    public static final String countries[]={"Nepal", "India", "China", "UK", "US", "Canada"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.lv);
+        ArrayAdapter countryApapter=new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                countries
+        );
+        listView.setAdapter(countryApapter);
     }
 }
